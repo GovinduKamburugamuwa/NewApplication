d10dacbc51b0c42385e4e37db237e8a79dfe19ba
GovinduKamburugamuwa <114232870+GovinduKamburugamuwa@users.noreply.github.comI>
2022-09-24 17:23:04 +0530

    first commit

---
+++ NewCommitApplication/src/newcommitapplication/NewCommitApplication.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newcommitapplication;
+
+/**
+ *
+ * @author MAX
+ */
+public class NewCommitApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Testing for the commit");
+    }
+    
+}
